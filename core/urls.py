2351c3b64d67c6a3b7e845cf89c374ac090e2b9b
rickyangel0625 <your_email@example.com>
2025-07-18 01:18:17 +0800

    Modify web page functionality

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 # core/urls.py
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView # 用於重定向，可選
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'), # 將根路徑映射到 post_list 視圖
+    # path('', views.post_list, name='post_list'), # 將根路徑映射到 post_list 視圖
+    path('', views.post_list_view, name='post_list'),
 ]
