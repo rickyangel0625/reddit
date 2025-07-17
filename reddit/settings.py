6dd8c55b484cf508b03e9421cd7428e2f207d779
rickyangel0625 <your_email@example.com>
2025-07-18 07:45:09 +0800

    up app setup-1

--- reddit/settings.py
+++ reddit/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
